7787e366a3c526edf551d8a7cb6fd3e6430ee0d3
Vasile Sebastian <zvoxty@gmail.com>
2016-12-07 11:18:18 +0200

    Update Functions.py

--- UniversityProblems/OOPPY 1/Functions.py
+++ UniversityProblems/OOPPY 1/Functions.py
@@ -3,33 +3,37 @@ from operator import itemgetter
 
 from math import *
 
+#Get rectangle coords
 def GetRectangleOfPolygon(polygon):
-    polygon.sort()
-    xMin = polygon[0][0]
-    xMax = polygon[len(polygon)-1][0]
+    polygon.sort() #Sort polygon coords by x 
+    xMin = polygon[0][0] #Get xMin = first element
+    xMax = polygon[len(polygon)-1][0] #Get xMax = last element
 
-    polygon.sort(key=lambda x: x[1])
+    polygon.sort(key=lambda x: x[1]) #Sort polygon by Y coord
 
-    yMin = polygon[0][1]
-    yMax = polygon[len(polygon)-1][1]
+    yMin = polygon[0][1] #Get yMin from first point
+    yMax = polygon[len(polygon)-1][1] #Get yMax from last point
 
-    rectangle = [[xMin,yMin],[xMax,yMin], [xMax,yMax],[xMin,yMax]]
+    rectangle = [[xMin,yMin],[xMax,yMin], [xMax,yMax],[xMin,yMax]] #Set rectangle points
 
-    return rectangle
+    return rectangle #return rectangle
 
+#Function used to get area of a polygon with n points
 def GetArea(polygon):
     area = 0
     for i in range(len(polygon)-1):
         area += polygon[i][0] * polygon[i + 1][1] - polygon[i + 1][0] * polygon[i][1]
     return fabs(area/2)
 
+#Function used to get the distance between two points
 def GetDistance(point1, point2):
     return sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
 
+#Function used to get the perimeter of a polygon
 def GetPolygonPerimeter(polygon):
     perimeter = 0
     for i in range(len(polygon)-1):
-        perimeter += GetDistance(polygon[i],polygon[i+1])
+        perimeter += GetDistance(polygon[i],polygon[i+1]) #Get distance between point 1 and point 2 and add to perimeter
 
     return perimeter
 
@@ -41,8 +45,8 @@ def GetInfo(polygon):
         Y = int(input("Insert Y:"))
         point = [X,Y]
         polygon.append(point)
-        polygon.sort()
 
+#Display graphic polygon using turtle library
 def DisplayPolygon(polygon):
     pu()
     for i in range(len(polygon)-1):
@@ -52,6 +56,7 @@ def DisplayPolygon(polygon):
 
     goto(polygon[0][0]*20,polygon[0][1]*20)
 
+#Function used to get gravity center of polygon
 def GetGravityCenter(polygon):
     x = 0
     y = 0
@@ -64,10 +69,12 @@ def GetGravityCenter(polygon):
 
     return [x,y]
 
+#Print polygon points on screen
 def PrintPolygon(polygon):
     for i in range(len(polygon)):
         print("Point ",i+1,":",polygon[i])
-
+        
+#Function used to compare area of two polygons
 def ComparePolygons(polygon1, polygon2):
     p1Area = GetArea(polygon1)
     p2Area = GetArea(polygon2)
@@ -77,6 +84,7 @@ def ComparePolygons(polygon1, polygon2):
     else:
         return "Aria dreptunghiului mai mare decat cea a poligonului"
 
+#Function used to print X and Y axes graphic using turtle
 def printGraph():
     pu()
     goto(0,-200)
